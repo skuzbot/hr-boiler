b98ee834eaf8a4f8014abfee310fbc1f7c60a16f
nickfredman <ntfredman@gmail.com>
2018-08-28 20:40:32 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>I am an app!</h1>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
